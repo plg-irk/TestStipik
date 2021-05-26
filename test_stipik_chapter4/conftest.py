a4af79f7a5449ac8409b248d0886ddeb0b1389e9
Evgeny <nk-irk@mail.ru>
2021-05-26 21:35:59 +0800

    Первоначальные установки исправление ввода

--- test_stipik_chapter4/conftest.py
+++ test_stipik_chapter4/conftest.py
@@ -4,14 +4,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):  # Объявляем параметр для запуска
-    parser.addoption('--user_language', action='store', default="en",
+    parser.addoption('--language', action='store', default="ru",
                      help="en or ru")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     print("\nStart test..")
-    user_language = request.config.getoption("user_language")
+    user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
--- test_stipik_chapter4/test_main_page.py
+++ test_stipik_chapter4/test_main_page.py
@@ -4,5 +4,3 @@ def test_guest_can_go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
-
-
