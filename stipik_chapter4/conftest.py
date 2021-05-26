6e93e794b5d4c054395c3e730ca90db3cd830d6a
Evgeny <nk-irk@mail.ru>
2021-05-27 02:15:38 +0800

    Добавляем два класса  и модифицируем test_main_page

--- stipik_chapter4/conftest.py
+++ stipik_chapter4/conftest.py
@@ -18,3 +18,4 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
+
--- stipik_chapter4/page/base_page.py
+++ stipik_chapter4/page/base_page.py
@@ -1,4 +1,5 @@
 class BasePage():
+
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
