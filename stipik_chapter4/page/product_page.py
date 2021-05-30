dd02338647a0bd9f1e2639aed40da055158d6811
Evgeny <nk-irk@mail.ru>
2021-05-30 13:44:44 +0800

    Добавляем проверку товара

--- stipik_chapter4/page/product_page.py
+++ stipik_chapter4/page/product_page.py
@@ -4,6 +4,10 @@ from .locators import MainPageLocators
 
 class ProductPage(BasePage):
 
+    def checking_product_availability(self):
+        self.is_element_present(*MainPageLocators.NAME_PRODUCT_OPTION)
+        self.is_element_present(*MainPageLocators.PRICE_PRODUCT_OPTION)
+
     def click_button_add_to_basket(self):
         button_add_backet = self.browser.find_element(*MainPageLocators.ADD_TO_BASKET)
         button_add_backet.click()
@@ -23,4 +27,12 @@ class ProductPage(BasePage):
     def prices_product_option_basket(self):
         prices_product_basket = self.browser.find_element(*MainPageLocators.PRICE_PRODUCT_OPTION_BASKET)
         return prices_product_basket
+
+    def checking_exeption(self):
+        assert self.name_product_option_basket().text == self.name_product_option().text,\
+            "Product is not option"
+                # Проверяем выбор товара и цену
+        assert self.prices_product_option_basket().text == self.prices_product_option().text,\
+            "Price is not equal"
+
 
--- stipik_chapter4/test_product_page.py
+++ stipik_chapter4/test_product_page.py
@@ -7,12 +7,8 @@ def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
 
+    page.checking_product_availability() # Проверяем присутствие товара и цену
     page.click_button_add_to_basket() # Добавляем товар в корзину
-
     page.solve_quiz_and_get_code() # Выполняем задание Stepik
+    page.checking_exeption() # Проверяем ожидания
 
-    assert page.name_product_option_basket().text == page.name_product_option().text,\
-        "Product is not option"
-            # Проверяем ожидания
-    assert page.prices_product_option_basket().text == page.prices_product_option().text,\
-        "Price is not equal"
