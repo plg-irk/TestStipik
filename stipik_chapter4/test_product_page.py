29fc67e9d30a4f3fd3c5cca01eeffe7eaee1d1d2
Evgeny <nk-irk@mail.ru>
2021-06-03 21:27:33 +0800

    Задание: наследование и отрицательные проверки (корректировка)

--- stipik_chapter4/test_product_page.py
+++ stipik_chapter4/test_product_page.py
@@ -1,11 +1,29 @@
 import pytest
-# from .page.main_page import MainPage
+from .page.main_page import MainPage
 from .page.product_page import ProductPage
 from .page.basket_page import BasketPage
 import time
 from .page.locators import PoductPageLocators
 from .page.locators import BasePageLocators
 
+
+class TestUserAddToBasketFromProductPage():
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        assert page.is_not_element_present(*PoductPageLocators.SUCCESS_MESSAGE), \
+            "Product is presented in the basket, but should not be"
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                      # открываем страницу
+        page.checking_product_availability() # Проверяем присутствие товара и цену
+        page.click_button_add_to_basket() # Добавляем товар в корзину
+        # page.solve_quiz_and_get_code() # Выполняем задание Stepik
+        page.checking_exeption() # Проверяем ожидания
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
@@ -64,6 +82,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
+    # Проверяем что гость переходит на страницу регистрации со страницы товара
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
